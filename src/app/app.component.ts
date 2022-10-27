777e49450fb4232ddc28622423fc7d09cd1b79c1
Dayane Gorgonha <dayanebismark18@gmail.com>
2022-10-27 16:55:07 -0300

    feat: creating profile page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { UsuariosServiceProvider } from './../providers/usuarios-service/usuarios-service';
+import { PerfilPage } from './../pages/perfil/perfil';
 import { LoginPage } from './../pages/login/login';
 import { ListaAgendamentosPage } from './../pages/lista-agendamentos/lista-agendamentos';
 import { Component, ViewChild } from '@angular/core';
@@ -14,20 +16,28 @@ export class MyApp {
   rootPage:any = LoginPage;
 
   public paginas = [
-    { titulo: 'Agendamentos', componente: ListaAgendamentosPage.name, icone: 'calendar' }
+    { titulo: 'Agendamentos', componente: ListaAgendamentosPage.name, icone: 'calendar' },
+    { titulo: 'Perfil', componente: PerfilPage.name, icone: 'person' }
   ];
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
+  constructor(platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    private _usuarioService: UsuariosServiceProvider) {
+      platform.ready().then(() => {
+        // Okay, so the platform is ready and our plugins are available.
+        // Here you can do any higher level native things you might need.
+        statusBar.styleDefault();
+        splashScreen.hide();
+      });
   }
 
   irParaPagina(componente) {
     this.nav.push(componente);
+  }
+
+  get usuarioLogado() {
+    return this._usuarioService.obtemUsuarioLogado();
   }
 }
 
